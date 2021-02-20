73a07c39c1814eb62a79d4992ca2854e887e6339
pablo <paulandrewjeffrey@gmail.com>
2021-02-19 20:54:44 -0800

    working pswrd generator

--- script.js
+++ script.js
@@ -1,16 +1,26 @@
+
 function generatePassword() {
-  var L = prompt ('How many characters?');
-  var password = '';
-  var charSet = 'abcdefghijklmnopqrstuvwxyz';
-  var charSetUpper = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  var charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
   var numSet = '0123456789';
   var spSet = '~!@#$%^&*';
-  
-  for (i=0; i<L; i++) {
-    var Y = Math.floor(Math.random()*charSet.length);
-    password += charSet.charAt(Y); 
-  }
-  return password;
+  var password = '';
+  var L = prompt ('How many characters?');
+  var U = prompt ('want some numbers? \n y/n?')
+    if (U == 'y') {
+      charSet.concat(charSet += numSet);
+      console.log(charSet);
+    }
+  var V = prompt ('Special Characters? \n y/n?')
+    if (V == 'y') {
+      charSet.concat(charSet += spSet);
+      console.log(charSet);
+    } 
+      for (i=0; i<L; i++) {
+      var Y = Math.floor(Math.random()*charSet.length);
+      password += charSet.charAt(Y); 
+      }
+      console.log(charSet);
+      return password;
 }
 
 console.log(generatePassword());
