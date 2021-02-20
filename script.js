4e86fb6f52d5bfe86eaef85b8f06c869e89bf643
pablo <paulandrewjeffrey@gmail.com>
2021-02-19 21:10:39 -0800

    modified spSet

--- script.js
+++ script.js
@@ -1,27 +1,37 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
-function generatePassword() {
-  var charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-  var numSet = '0123456789';
-  var spSet = '~!@#$%^&*';
-  var password = '';
-  var L = prompt ('How many characters?');
-  var U = prompt ('want some numbers? \n y/n?')
-    if (U == 'y') {
-      charSet.concat(charSet += numSet);
-      console.log(charSet);
-    }
-  var V = prompt ('Special Characters? \n y/n?')
-    if (V == 'y') {
-      charSet.concat(charSet += spSet);
-      console.log(charSet);
-    } 
-      for (i=0; i<L; i++) {
-      var Y = Math.floor(Math.random()*charSet.length);
-      password += charSet.charAt(Y); 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+  passwordText.value = password;
+  function generatePassword() {
+    var charSet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    var numSet = '0123456789';
+    var spSet = '!"#$%&\'()*+,-./:;<=>?@[\]^_`{|}~';
+    var password = '';
+    var L = prompt ('How many characters?');
+    var U = prompt ('want some numbers? \n y/n?')
+      if (U == 'y') {
+        charSet.concat(charSet += numSet);
+        console.log(charSet);
       }
-      console.log(charSet);
-      return password;
+    var V = prompt ('Special Characters? \n y/n?')
+      if (V == 'y') {
+        charSet.concat(charSet += spSet);
+        console.log(charSet);
+      } 
+        for (i=0; i<L; i++) {
+        var Y = Math.floor(Math.random()*charSet.length);
+        password += charSet.charAt(Y); 
+        }
+        console.log(charSet);
+        return password;
+  }
 }
 
-console.log(generatePassword());
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
 
